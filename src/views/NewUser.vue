095a20316e58b945035d7d62fbe89c68ca020491
christian <christian.coley@hotmail.fr>
2021-10-16 11:13:07 +0200

    add/firstView

---
+++ src/views/NewUser.vue
@@ -0,0 +1,18 @@
+<template>
+<h1> Hello tout le monde </h1>
+
+</template>
+
+<script lang="ts">
+
+
+    export default {
+        name: 'NewUser',
+        components: {},
+        setup() {
+            return {
+
+            }
+        }
+    }
+</script>
